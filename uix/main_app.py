a3ee8077bae9b3d6a46d8fcd17e3b10888c36434
Gayfut <gayfut1@gmail.com>
2021-03-01 02:54:28 +0000

    v0.001.02(change idea, finish parser, need comments)

--- uix/main_app.py
+++ uix/main_app.py
@@ -6,6 +6,7 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.graphics import Color, Rectangle
 
+from parser.parser import Parser
 from settings import PATH_TO_IMAGES
 
 
@@ -16,14 +17,21 @@ class MainApp(App):
             Color(1, 1, 1)
             Rectangle(pos=layout.pos, size=(10000, 10000))
 
-        kgeu_image = Image(source=f"{PATH_TO_IMAGES}kgeu_logo.png", size_hint=(.4, .4), pos_hint={'center_x': .5, 'center_y': .7})
-        group_input = TextInput(text="Please entry your group!", background_color=[0,0,0,1], cursor_color=[1,1,1,1], foreground_color=[1,1,1,1], multiline=False, size_hint=(.4, .05), pos_hint={'center_x': .5, 'center_y': .4})
-        enter_button = Button(text='Show schedule', background_color=[0,0,0,1], size_hint=(.2, .05), pos_hint={'center_x': .5, 'center_y': .3})
+        rap_image = Image(source=f"{PATH_TO_IMAGES}rap_logo.png", size_hint=(.4, .4), pos_hint={'center_x': .5, 'center_y': .7})
+        password_input = TextInput(text="Please enter password!", background_color=[0,0,0,1], cursor_color=[1,1,1,1], foreground_color=[1,1,1,1], halign='center', border=(1, 1, 1, 1), multiline=False, size_hint=(.4, .05), pos_hint={'center_x': .5, 'center_y': .4})
+        enter_button = Button(text='Show new albums', background_color=[0,0,0,1], size_hint=(.2, .05), pos_hint={'center_x': .5, 'center_y': .3})
         copyright_label = Label(text="Created by Gayfut", color=[0,0,0,1], size_hint=(.3, .05), pos_hint={'center_x': .5, 'center_y': .03})
 
-        layout.add_widget(kgeu_image)
-        layout.add_widget(group_input)
+        layout.add_widget(rap_image)
+        layout.add_widget(password_input)
         layout.add_widget(enter_button)
         layout.add_widget(copyright_label)
 
+        enter_button.bind(on_press=self.on_press_button)
+
         return layout
+
+    def on_press_button(self, instance):
+        parser = Parser()
+        parser.start_parse(2)
+        parser.stop_parse()
