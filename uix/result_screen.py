2fb4e6b3587f6b90566f4e6b7288fdc75ac1f04e
Gayfut <gayfut1@gmail.com>
2021-03-12 13:27:11 +0000

    v0.001.07(work on showing results, need comments)

--- uix/result_screen.py
+++ uix/result_screen.py
@@ -1,6 +1,6 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.properties import StringProperty, ObjectProperty
+from kivy.properties import StringProperty
 from requests import get
 
 from tokens import site_url
@@ -18,23 +18,94 @@ Builder.load_string(
         Button:
             text: 'Back'
             background_color: [0, 0, 0, 1]
-            size_hint: (0.05, 0.05)
-            pos_hint: {"center_x": 0.95, "center_y": 0.95}
+            size_hint: (0.1, 0.05)
+            pos_hint: {"center_x": 0.9, "center_y": 0.95}
             on_press: root.manager.current = 'main'
+        Button:
+            text: 'Next'
+            background_color: [0, 0, 0, 1]
+            size_hint: (0.1, 0.05)
+            pos_hint: {"center_x": 0.3, "center_y": 0.95}
+            on_press: root.next_album()
+        Button:
+            text: 'Prev'
+            background_color: [0, 0, 0, 1]
+            size_hint: (0.1, 0.05)
+            pos_hint: {"center_x": 0.3, "center_y": 0.05}
+            on_press: root.prev_album()
+        Label:
+            text: root.album_title
+            color: [0, 0, 0, 1]
+            font_size: 10
+            size_hint: (0.1, 0.05)
+            pos_hint: {"center_x": 0.3, "center_y": 0.9}
+        Label:
+            text: root.album_rating
+            color: [0, 0, 0, 1]
+            font_size: 10
+            size_hint: (0.1, 0.05)
+            pos_hint: {"center_x": 0.3, "center_y": 0.85}
+        Label:
+            text: root.album_link_to_download
+            color: [0, 0, 0, 1]
+            font_size: 10
+            size_hint: (0.1, 0.05)
+            pos_hint: {"center_x": 0.3, "center_y": 0.8}
+        Label:
+            text: root.album_link_to_album
+            color: [0, 0, 0, 1]
+            font_size: 10
+            size_hint: (0.1, 0.05)
+            pos_hint: {"center_x": 0.3, "center_y": 0.75}
+        Label:
+            text: root.album_track_list
+            color: [0, 0, 0, 1]
+            font_size: 10
+            size_hint: (0.1, 0.05)
+            pos_hint: {"center_x": 0.8, "center_y": 0.5}
 """
 )
 
 
 class ResultScreen(Screen):
 
-    album_info = StringProperty(defaultvalue="No data")
+    album_index = 0
+    album_title = StringProperty(defaultvalue="No title")
+    album_rating = StringProperty(defaultvalue="No rating")
+    album_track_list = StringProperty(defaultvalue="No tracks")
+    album_link_to_download = StringProperty(defaultvalue="No download link")
+    album_link_to_album = StringProperty(defaultvalue="No link to album")
 
-    # def show_album(self):
-    #     self.album_info = self.__get_albums_list()
-    #
-    # @staticmethod
-    # def __get_albums_list():
-    #     albums = get(site_url)
-    #     albums = albums.json()
-    #
-    #     return albums
+    def next_album(self):
+        albums = self.__get_albums_list()
+
+        try:
+            self.album_title = albums[self.album_index+1]['title']
+            self.album_rating = albums[self.album_index + 1]['rating']
+            self.album_track_list = albums[self.album_index + 1]['track_list']
+            self.album_link_to_download = albums[self.album_index + 1]['link_to_download']
+            self.album_link_to_album = albums[self.album_index + 1]['link_to_album']
+        except IndexError:
+            self.album_index = 0
+
+        self.album_index += 1
+
+    def prev_album(self):
+        albums = self.__get_albums_list()
+
+        try:
+            self.album_title = albums[self.album_index-1]['title']
+            self.album_rating = albums[self.album_index-1]['rating']
+            self.album_track_list = albums[self.album_index-1]['track_list']
+            self.album_link_to_download = albums[self.album_index-1]['link_to_download']
+            self.album_link_to_album = albums[self.album_index-1]['link_to_album']
+        except IndexError:
+            self.album_index = 0
+
+        self.album_index -= 1
+
+    @staticmethod
+    def __get_albums_list():
+        albums = get(site_url).json()
+
+        return albums
